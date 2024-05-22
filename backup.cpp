ce3f18d0f0672b2246034519eb98b8681850786b
duongngockhanh <khanhduongsc@gmail.com>
2024-05-22 15:12:49 +0700

    demo some files

---
+++ backup.cpp
@@ -0,0 +1,35 @@
+#include <torch/script.h> // One-stop header.
+
+#include <iostream>
+#include <memory>
+
+int main(int argc, const char* argv[]) {
+  if (argc != 2) {
+    std::cerr << "usage: example-app <path-to-exported-script-module>\n";
+    return -1;
+  }
+
+
+  torch::jit::script::Module module;
+  try {
+    // Deserialize the ScriptModule from a file using torch::jit::load().
+    module = torch::jit::load(argv[1]);
+  }
+  catch (const c10::Error& e) {
+    std::cerr << "error loading the model\n";
+    return -1;
+  }
+
+  std::cout << "ok\n";
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <torch/torch.h>
+
+int main(){
+    torch::Tensor x = torch::randn({3, 3});
+    std::cout << x << std::endl;
+    // std::cout << "Khanh abc.\n";
+}
